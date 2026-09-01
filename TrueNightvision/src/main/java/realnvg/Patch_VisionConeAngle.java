25603bedfeffa025a11b2a8d432bed3a019c45dd
Danya <57011718+7H40@users.noreply.github.com>
2026-09-01 17:43:17 +0300

    я че ебанутый

---
+++ TrueNightvision/src/main/java/realnvg/Patch_VisionConeAngle.java
@@ -0,0 +1,30 @@
+package com.eich.realnvg;
+import me.zed_0xff.zombie_buddy.Patch;
+import zombie.characters.IsoGameCharacter;
+import zombie.characters.IsoPlayer;
+import java.lang.reflect.Method;
+@Patch(className = "zombie.vispoly.VisibilityPolygon2", methodName = "calculateVisionCone", warmUp = true)
+public class Patch_VisionConeAngle {
+    private static Method degreesToConeMethod;
+    @Patch.OnExit
+    public static void onExit(
+            @Patch.Argument(0) IsoGameCharacter player,
+            @Patch.Return(readOnly = false) float returnValue) {
+        if (NVGState.nvgEnabled <= 0.5f) {
+            return;
+        }
+        if (!(player instanceof IsoPlayer) || player != IsoPlayer.getInstance()) {
+            return;
+        }
+        try {
+            if (degreesToConeMethod == null) {
+                Class<?> clazz = Class.forName("zombie.vispoly.VisibilityPolygon2");
+                degreesToConeMethod = clazz.getDeclaredMethod("degreesToCone", float.class);
+                degreesToConeMethod.setAccessible(true);
+            }
+            returnValue = (Float) degreesToConeMethod.invoke(null, NVGState.coneAngle);
+        } catch (Exception e) {
+            //aaaaaaaa
+        }
+    }
+}
